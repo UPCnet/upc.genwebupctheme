2cee70534058b1dde8f313e32074932db7794e48
victor.fernandez <victor.fernandez@6deaf63b-a510-49cf-aad4-8c651776ecce>
2008-05-07 09:35:28 +0000

    modificacions capsalera superior
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genwebupctheme@45 6deaf63b-a510-49cf-aad4-8c651776ecce

--- upc/genwebupctheme/browser/capsalera.py
+++ upc/genwebupctheme/browser/capsalera.py
@@ -1,8 +1,15 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
+from zope.component import getMultiAdapter
 
+from plone.app.layout.viewlets import common
 
-class capsaleraSuperior(ViewletBase):
+class capsaleraSuperior(common.SiteActionsViewlet, common.SearchBoxViewlet):
     render = ViewPageTemplateFile('capsaleraSuperior.pt')
-#
+
 #    def update(self):
+#        context_state = getMultiAdapter((self.context, self.request),
+#                                        name=u'plone_context_state')
+#        self.site_actions = context_state.actions().get('site_actions', None)
+
+        
