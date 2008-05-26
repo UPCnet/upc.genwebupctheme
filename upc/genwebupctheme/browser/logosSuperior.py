016b6a6b3f5ebd76aa5755f499e030fa3afbac75
roberto.diaz <roberto.diaz@6deaf63b-a510-49cf-aad4-8c651776ecce>
2008-05-26 16:02:28 +0000

    Capsalera amb banners, login i search. Amb tabs a mitjes!
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genwebupctheme@74 6deaf63b-a510-49cf-aad4-8c651776ecce

--- upc/genwebupctheme/browser/logosSuperior.py
+++
@@ -1,6 +0,0 @@
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.app.layout.viewlets.common import ViewletBase
-from Products.CMFCore.utils import getToolByName
-
-class logosSuperior(ViewletBase):
-    render = ViewPageTemplateFile('logosSuperior.pt')
