e1c69d5921d3e05b646ab1656f393d694d39c09d
roberto.diaz <roberto.diaz@6deaf63b-a510-49cf-aad4-8c651776ecce>
2008-06-06 08:03:26 +0000

    eines unificades
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genwebupctheme@89 6deaf63b-a510-49cf-aad4-8c651776ecce

--- upc/genwebupctheme/browser/capsalera.py
+++ upc/genwebupctheme/browser/capsalera.py
@@ -4,6 +4,21 @@ from plone.memoize.instance import memoize
 from zope.component import getMultiAdapter
 from Products.CMFCore.utils import getToolByName
 from plone.app.layout.viewlets import common
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.layout.viewlets.common import ViewletBase
+from zope.interface import implements, alsoProvides
+from zope.component import getMultiAdapter
+from zope.viewlet.interfaces import IViewlet
+from zope.deprecation.deprecation import deprecate
+from plone.app.layout.globals.interfaces import IViewView 
+from AccessControl import getSecurityManager
+from Acquisition import aq_base, aq_inner
+from Products.CMFPlone.utils import safe_unicode
+from Products.Five.browser import BrowserView
+#from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+#from Products.CMFCore.utils import getToolByName
+from cgi import escape
+from urllib import quote_plus
 
 class capsaleraSuperior(ViewletBase):
     render = ViewPageTemplateFile('capsaleraSuperior.pt')
@@ -36,77 +51,39 @@ class logosSuperior(ViewletBase):
 class logoNeutre(ViewletBase):
     render = ViewPageTemplateFile('logoNeutre.pt')
 
-class portletLogin(ViewletBase):
-
-    def __init__(self, context, request, view, manager):
-        ViewletBase.__init__(self, context, request, view, manager)
-        
-        self.membership = getToolByName(self.context, 'portal_membership')
-        
-        self.context_state = getMultiAdapter((context, request), name=u'plone_context_state')
-        self.portal_state = getMultiAdapter((context, request), name=u'plone_portal_state')
-        self.pas_info = getMultiAdapter((context, request), name=u'pas_info')
-
-    def show(self):
-        if not self.portal_state.anonymous():
-            return False
-        if not self.pas_info.hasLoginPasswordExtractor():
-            return False
-        page = self.request.get('URL', '').split('/')[-1]
-        return page not in ('login_form', 'join_form')
-
-    @property
-    def available(self):
-        return self.auth() is not None and self.show()
-
-    def login_form(self):
-        return '%s/login_form' % self.portal_state.portal_url()
-
-    def mail_password_form(self):
-        return '%s/mail_password_form' % self.portal_state.portal_url()
-
-    def login_name(self):
-        auth = self.auth()
-        name = None
-        if auth is not None:
-            name = getattr(auth, 'name_cookie', None)
-        if not name:
-            name = '__ac_name'
-        return name
-
-    def login_password(self):
-        auth = self.auth()
-        passwd = None
-        if auth is not None:
-            passwd = getattr(auth, 'pw_cookie', None)
-        if not passwd:
-            passwd = '__ac_password'
-        return passwd
-
-    def join_action(self):
-        userActions = self.context_state.actions()['user']
-        joinAction = [a['url'] for a in userActions if a['id'] == 'join']
-        if len(joinAction) > 0:
-            return joinAction.pop()
-        else:
-            return None
-
-    def can_register(self):
-        if getToolByName(self.context, 'portal_registration', None) is None:
-            return False
-        return self.membership.checkPermission('Add portal member', self.context)
-
-    def can_request_password(self):
-        return self.membership.checkPermission('Mail forgotten password', self.context)
-
-    @memoize
-    def auth(self, _marker=[]):
-        acl_users = getToolByName(self.context, 'acl_users')
-        return getattr(acl_users, 'credentials_cookie_auth', None)
-
-    def update(self):
-        pass
-
-    render = ViewPageTemplateFile('portletLogin.pt')
+class eines(ViewletBase):
+    render = ViewPageTemplateFile('eines.pt')
     
+    def update(self):
+        super(eines, self).update()
 
+        context_state = getMultiAdapter((self.context, self.request),
+                                        name=u'plone_context_state')
+        tools = getMultiAdapter((self.context, self.request), name=u'plone_tools')
+        
+        sm = getSecurityManager()
+
+        self.user_actions = context_state.actions().get('user', None)
+
+        plone_utils = getToolByName(self.context, 'plone_utils')
+        self.getIconFor = plone_utils.getIconFor
+
+        self.anonymous = self.portal_state.anonymous()
+
+        if not self.anonymous:
+        
+            member = self.portal_state.member()
+            userid = member.getId()
+            
+            if sm.checkPermission('Portlets: Manage own portlets', self.context):
+                self.homelink_url = self.site_url + '/dashboard'
+            else:
+                self.homelink_url = self.site_url + '/author/' + quote_plus(userid)
+            
+            member_info = tools.membership().getMemberInfo(member.getId())
+            fullname = member_info.get('fullname', '')
+            if fullname:
+                self.user_name = fullname
+            else:
+                self.user_name = userid
+    
