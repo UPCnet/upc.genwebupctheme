f5c82ef0301f69b2ae44276e7587cab9bc8fe23e
roberto.diaz <roberto.diaz@6deaf63b-a510-49cf-aad4-8c651776ecce>
2008-07-01 17:35:20 +0000

    homepage modified
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genwebupctheme@102 6deaf63b-a510-49cf-aad4-8c651776ecce

--- upc/genwebupctheme/browser/capsalera.py
+++ upc/genwebupctheme/browser/capsalera.py
@@ -157,22 +157,3 @@ class eines(ViewletBase):
             else:
                 self.user_name = userid
 
-class GlobalSectionsViewlet(ViewletBase):
-    render = ViewPageTemplateFile('globalsections.pt')
-    
-    def update(self):
-        context_state = getMultiAdapter((self.context, self.request),
-                                        name=u'plone_context_state')
-                
-        actions = context_state.actions()
-     
-        portal_tabs_view = getMultiAdapter((self.context, self.request),
-                                           name='portal_tabs_view')
-        self.portal_tabs = portal_tabs_view.topLevelTabs(actions=actions)
-        
-        selectedTabs = self.context.restrictedTraverse('selectedTabs')
-  
-        self.selected_tabs = selectedTabs('index_html',
-                                          self.context,
-                                          self.portal_tabs)
-        self.selected_portal_tab = self.selected_tabs['portal']
