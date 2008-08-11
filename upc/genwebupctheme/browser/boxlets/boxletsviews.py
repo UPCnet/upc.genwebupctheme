f3d2847a5a903aca444fafa6e702f25f7fa1d020
victor.fernandez <victor.fernandez@6deaf63b-a510-49cf-aad4-8c651776ecce>
2008-08-11 13:55:34 +0000

    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genwebupctheme@195 6deaf63b-a510-49cf-aad4-8c651776ecce

--- upc/genwebupctheme/browser/boxlets/boxletsviews.py
+++ upc/genwebupctheme/browser/boxlets/boxletsviews.py
@@ -116,41 +116,12 @@ class agenda(BrowserView,calendar_render):
                        sort_on='start',
                        sort_limit=limit)[:limit]
 
-class agendamini(BrowserView):
-#    template=ViewPageTemplateFile('agenda.pt')
-    
-    def __init__(self, context, request):
-        self.context = context
-        self.request = request
-
-        self.utils = getMultiAdapter((self.context, self.request),
-                                        name=u'upc.genweb.utils')
-        self.now = localtime()
-        
-    def mes(self, mes):
-        return self.utils.mes(mes)
-    
-    def dia_semana(self, dia):
-        return self.utils.dia_semana(dia)
-    
-    def getRSS (self):
-        items = []
-        url = 'http://www.upc.edu/catala/RSS/actualitatUpc.php'
-        d = feedparser.parse(url)
-        for item in d['items']:
-            try:
-                link = item.links[0]['href']
-                itemdict = {
-                    'title' : item.title,
-                    'url' : link,
-                    'summary' : item.get('description',''),
-                }
-            except AttributeError:
-                continue
-            items.append(itemdict)
-        return items
-
+class agendamini(agenda):
+    _template = ViewPageTemplateFile('agenda_mini.pt')
    
+class agendaCalendariPetit(agenda):
+    _template = ViewPageTemplateFile('agenda_calendari_petit.pt')
+
 class noticies(BrowserView):
 #    template = ViewPageTemplateFile('noticies.pt')
 
