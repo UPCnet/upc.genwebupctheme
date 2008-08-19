4ee946471d59111b60a3fef7003e3880d40c869a
victor.fernandez <victor.fernandez@6deaf63b-a510-49cf-aad4-8c651776ecce>
2008-08-19 12:35:53 +0000

    Inclusió de altres sabors (Unitats), creació de interficies, etc... Altres canvis en les document actions.
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genwebupctheme@218 6deaf63b-a510-49cf-aad4-8c651776ecce

--- upc/genwebupctheme/browser/interfaces.py
+++ upc/genwebupctheme/browser/interfaces.py
@@ -1,8 +1,9 @@
 from plone.theme.interfaces import IDefaultPloneLayer
 
-class IThemeSpecificNeutre(IDefaultPloneLayer):
-    """Marker interface that defines a Zope 3 browser layer.
-       If you need to register a viewlet only for the
-       "Tema genwebUPC" theme, this interface must be its layer
-       (in genwebupctheme/viewlets/configure.zcml).
+class IThemeSpecificNeutre3(IDefaultPloneLayer):
+    """Marker interface del sabor Neutre3
+    """
+
+class IThemeSpecificUnitats(IDefaultPloneLayer):
+    """Marker interface del sabor Unitats
     """
