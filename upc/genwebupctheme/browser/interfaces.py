9668b3124e84dab20816c7af26f72b19ae448155
victor.fernandez <victor.fernandez@6deaf63b-a510-49cf-aad4-8c651776ecce>
2008-10-02 09:50:58 +0000

    nuevos skins con los sabores
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genwebupctheme@280 6deaf63b-a510-49cf-aad4-8c651776ecce

--- upc/genwebupctheme/browser/interfaces.py
+++ upc/genwebupctheme/browser/interfaces.py
@@ -4,6 +4,18 @@ class IThemeSpecificNeutre3(IDefaultPloneLayer):
     """Marker interface del sabor Neutre3
     """
 
-class IThemeSpecificUnitats(IDefaultPloneLayer):
-    """Marker interface del sabor Unitats
+class IThemeSpecificUnitat(IDefaultPloneLayer):
+    """Marker interface del sabor Unitat
+    """
+
+class IThemeSpecificNeutre2(IDefaultPloneLayer):
+    """Marker interface del sabor Neutre2
+    """
+
+class IThemeSpecificMaster(IDefaultPloneLayer):
+    """Marker interface del sabor Master
+    """
+
+class IThemeSpecificPortal(IDefaultPloneLayer):
+    """Marker interface del sabor Portal
     """
