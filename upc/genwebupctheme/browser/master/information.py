49cd29af6ebf09923a02ec658cb9403c04ca3cef
jose.luis.vivanco <jose.luis.vivanco@6deaf63b-a510-49cf-aad4-8c651776ecce>
2008-10-30 11:28:37 +0000

    MASTERS: Pagina competencies.
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genwebupctheme@406 6deaf63b-a510-49cf-aad4-8c651776ecce

--- upc/genwebupctheme/browser/master/information.py
+++ upc/genwebupctheme/browser/master/information.py
@@ -4,3 +4,5 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 class infoGeneralView(BrowserView):
     __call__=ViewPageTemplateFile('info-general.pt')
 
+class competenciesView(BrowserView):
+    __call__=ViewPageTemplateFile('competencies.pt')
