2dac9752043e1f61b72c5d9c2ab384193f1fccee
jose.luis.vivanco <jose.luis.vivanco@6deaf63b-a510-49cf-aad4-8c651776ecce>
2008-10-30 12:14:41 +0000

    MASTERS: Pagina competencies.
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genwebupctheme@408 6deaf63b-a510-49cf-aad4-8c651776ecce

--- upc/genwebupctheme/browser/utils.py
+++ upc/genwebupctheme/browser/utils.py
@@ -270,6 +270,24 @@ class utilitats(BrowserView):
         
         return self.remapList2Dic(dictKeys,results)
 
+
+    def getMasterCompentencies(self, id_titulacio, id_estudi):
+        
+        db = self.connectDatabase()
+        c=db.cursor()     
+        c.execute("""SELECT sortides_professionals_cat,sortides_professionals_cat,sortides_professionals_ing,m_competencies_cat, m_competencies_esp, m_competencies_ing FROM upc_titulacio,upc_titulacio_orientacio, upc_presencialitat up, upc_titulacio_plus plus,upc_estudi_plus plusEstudi, upc_estudi LEFT JOIN upc_unitat unitat ON upc_estudi.m_uni_coordinadora=unitat.id_unitat WHERE upc_titulacio.id_titulacio=%s AND upc_estudi.id_estudi=%s AND upc_titulacio_orientacio.m_id_orientacio=upc_titulacio.m_id_orientacio AND upc_estudi.id_presencialitat=up.id_presencialitat AND plus.id_titulacio=%s AND plusEstudi.id_estudi=%s""", (id_titulacio,id_estudi,id_titulacio,id_estudi,))
+        results = c.fetchone()
+        dictKeys = (    
+                    'sortides_professionals_cat',  
+                    'sortides_professionals_esp',  
+                    'sortides_professionals_ing',  
+                    'm_competencies_cat', 
+                    'm_competencies_esp', 
+                    'm_competencies_ing', 
+                    )
+
+        return self.remapList2Dic(dictKeys,results)
+
     def select_idiomes(self, id_estudi):
         
         db = self.connectDatabase()
