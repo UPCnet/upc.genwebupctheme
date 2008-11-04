50305dbf0504cb656239b17dd166f7362dc9fa86
victor.fernandez <victor.fernandez@6deaf63b-a510-49cf-aad4-8c651776ecce>
2008-11-04 13:36:25 +0000

    Carpetes ocultes si anonim, problema del candauet per elements interns.
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genwebupctheme@426 6deaf63b-a510-49cf-aad4-8c651776ecce

--- upc/genwebupctheme/browser/dynamic.py
+++ upc/genwebupctheme/browser/dynamic.py
@@ -1,5 +1,18 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.Five.browser import BrowserView
+from zope.component import getMultiAdapter
 
 class DynamicCssView(BrowserView):
     __call__ = ViewPageTemplateFile('dynamic.css.pt')
+
+    def portal_url(self):
+        """ Funcion a que retorna el path 
+        """
+        portal_state = getMultiAdapter((self.context, self.request),
+                                    name=u'plone_portal_state')
+        return portal_state.portal_url()
+    
+    def es_anonim(self):
+        context_state = getMultiAdapter((self.context, self.request),
+                                    name=u'plone_portal_state')
+        return context_state.anonymous()        
