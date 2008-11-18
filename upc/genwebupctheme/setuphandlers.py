419d6b3bf62e9d0eaa3fb36746cdd3af628a5c4d
carles.bruguera <carles.bruguera@6deaf63b-a510-49cf-aad4-8c651776ecce>
2008-11-18 09:07:46 +0000

    Sobreescriptura imatges fckeditor, comprova ultima versio i ubicacio dels eggs
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genwebupctheme@500 6deaf63b-a510-49cf-aad4-8c651776ecce

--- upc/genwebupctheme/setuphandlers.py
+++ upc/genwebupctheme/setuphandlers.py
@@ -4,10 +4,19 @@ def setupVarious(context):
     # Here, we are not parsing an XML file, but we use this text file as a
     # flag to check that we actually meant for this import step to be run.
     # The file is found in profiles/default.
-
     if context.readDataFile('upc.genwebupctheme_various.txt') is None:
         pass
-#        import os,shutil
-#        buildout_folder = os.getcwd()
-#        shutil.copyfile('%s/src/upc.genwebupctheme/upc/genwebupctheme/skins/upc_genwebupctheme_custom_images/fck_strip.gif' % (buildout_folder),
-#                    '%s/eggs/Products.FCKeditor-2.6.3.1-py2.4.egg/Products/FCKeditor/skins/fckeditor/editor/skins/silver/fck_strip.gif' % (buildout_folder))
+    import os,shutil
+
+    #busquem l'ultima versio del fckeditor
+    buildout_folder = os.getcwd()
+    fckeditor_eggs = [dir for dir in os.listdir('%s/eggs/' % buildout_folder) if dir.upper().find('FCKEDITOR')>0]
+    fckeditor_eggs.sort()
+    fckeditor_path = fckeditor_eggs[-1]
+    
+    #busquem on tenim el tema
+    genwebtheme_source = os.path.exists('%s/src/upc.genwebupctheme/' % buildout_folder) and 'src' or 'eggs'
+    
+    #sobreescrivim l'fckstrip
+    shutil.copyfile('%s/%s/upc.genwebupctheme/upc/genwebupctheme/skins/upc_genwebupctheme_custom_images/fck_strip.gif' % (buildout_folder,genwebtheme_source),
+                '%s/eggs/%s/Products/FCKeditor/skins/fckeditor/editor/skins/silver/fck_strip.gif' % (buildout_folder,fckeditor_path)) 
