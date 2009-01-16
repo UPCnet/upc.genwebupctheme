d1e313b660a1b9bd767299f25145be4d1327dc87
ramon.navarro-bosch <ramon.navarro-bosch@6deaf63b-a510-49cf-aad4-8c651776ecce>
2009-01-16 12:23:32 +0000

    Ara siiii monkeyyyy patching events power
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genwebupctheme@721 6deaf63b-a510-49cf-aad4-8c651776ecce

--- upc/genwebupctheme/events.py
+++ upc/genwebupctheme/events.py
@@ -5,6 +5,7 @@ from upc.genwebupctheme.browser.interfaces import IgenWebControlPanelSchemaGener
 
 def folderAdded(folder,event):
     if ISiteRoot.providedBy(folder.aq_parent):
+        folder.context = folder.aq_parent
         gw_util = utils.getGWConfig(folder)
         folder.setConstrainTypesMode(1)
         folder.setLocallyAllowedTypes(tuple([i.value for i in gw_schema._v_attrs['constrains'].value_type.vocabulary.__iter__()]))
