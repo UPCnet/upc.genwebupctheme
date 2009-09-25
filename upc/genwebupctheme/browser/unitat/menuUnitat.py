d6873da874d8d6b6ea5d2decf85bddaf191e524b
jose.luis.vivanco <jose.luis.vivanco@6deaf63b-a510-49cf-aad4-8c651776ecce>
2009-09-25 11:16:57 +0000

    menu unitats
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genwebupctheme@1317 6deaf63b-a510-49cf-aad4-8c651776ecce

--- upc/genwebupctheme/browser/unitat/menuUnitat.py
+++ upc/genwebupctheme/browser/unitat/menuUnitat.py
@@ -9,6 +9,8 @@ from Products.Five.browser import BrowserView
 from plone.app.layout.viewlets.common import GlobalSectionsViewlet
 
 from Products.CMFPlone.interfaces.siteroot import IPloneSiteRoot
+from Products.CMFCore.utils import getToolByName
+
 
 class GlobalSections(GlobalSectionsViewlet):
     index = ViewPageTemplateFile('menuUnitat.pt')
@@ -33,3 +35,25 @@ class GlobalSections(GlobalSectionsViewlet):
         if esUltim:
             return '100%'
         
+    def UnitatsPortal_tabs(self):
+        urltool = getToolByName(self.context, 'portal_url')
+        portal_catalog = getToolByName(self.context, 'portal_catalog')
+        context_state = getMultiAdapter((self.context, self.request),
+                                        name=u'plone_context_state')
+        
+        path = urltool.getPortalPath()        
+        folders = portal_catalog.searchResults(portal_type = 'Seccio',                                                                                       
+                                               path = dict(query=path, depth=1),     
+                                               review_state=['internally_published','external','published'],
+                                               sort_on='getObjPositionInParent')           
+        
+        results = []
+        for fold in folders:  
+            if fold.exclude_from_nav == False:                  
+                results.append(dict(name = fold.getObject().getText,
+                                    url = fold.getURL(),
+                                    id = fold.getId,
+                                    description = fold.Description
+                                   )
+                               )
+        return results      
