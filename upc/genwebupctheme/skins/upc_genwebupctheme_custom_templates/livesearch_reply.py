57c78e45673b8e5867262bcfecf290d9fa9d23dd
Víctor Fernández de Alba <sneridagh@gmail.com>
2011-02-23 15:45:15 +0100

    ajustos finals, primera versio decent dels sabors, revisats tots els templates.

--- upc/genwebupctheme/skins/upc_genwebupctheme_custom_templates/livesearch_reply.py
+++ upc/genwebupctheme/skins/upc_genwebupctheme_custom_templates/livesearch_reply.py
@@ -19,6 +19,7 @@ ploneUtils = getToolByName(context, 'plone_utils')
 portal_url = getToolByName(context, 'portal_url')()
 pretty_title_or_id = ploneUtils.pretty_title_or_id
 plone_view = context.restrictedTraverse('@@plone')
+portal_state = context.restrictedTraverse('@@plone_portal_state')
 
 portalProperties = getToolByName(context, 'portal_properties')
 siteProperties = getattr(portalProperties, 'site_properties', None)
@@ -28,7 +29,6 @@ if siteProperties is not None:
 
 # SIMPLE CONFIGURATION
 USE_ICON = True
-USE_RANKING = False
 MAX_TITLE = 29
 MAX_DESCRIPTION = 93
 
@@ -57,10 +57,11 @@ def quote_bad_chars(s):
 # so we don't even attept to make that right.
 # But we strip these and these so that the catalog does
 # not interpret them as metachars
-##q = re.compile(r'[\*\?\-\+]+').sub(' ', q)
-for char in '?-+*':
+# See http://dev.plone.org/plone/ticket/9422 for an explanation of '\u3000'
+multispace = u'\u3000'.encode('utf-8')
+for char in ('?', '-', '+', '*', multispace):
     q = q.replace(char, ' ')
-r=q.split()
+r = q.split()
 r = " AND ".join(r)
 r = quote_bad_chars(r)+'*'
 searchterms = url_quote_plus(r)
@@ -68,11 +69,13 @@ searchterms = url_quote_plus(r)
 site_encoding = context.plone_utils.getSiteEncoding()
 if path is None:
     path = getNavigationRoot(context)
-results = catalog(SearchableText=r, portal_type=friendly_types, path=path)
+results = catalog(SearchableText=r, portal_type=friendly_types, path=path,
+    sort_limit=limit)
 
 searchterm_query = '?searchterm=%s'%url_quote_plus(q)
 
-RESPONSE = context.REQUEST.RESPONSE
+REQUEST = context.REQUEST
+RESPONSE = REQUEST.RESPONSE
 RESPONSE.setHeader('Content-Type', 'text/xml;charset=%s' % site_encoding)
 
 # replace named entities with their numbered counterparts, in the xml the named ones are not correct
@@ -81,7 +84,7 @@ RESPONSE.setHeader('Content-Type', 'text/xml;charset=%s' % site_encoding)
 legend_livesearch = _('legend_livesearch', default='LiveSearch &#8595;')
 label_no_results_found = _('label_no_results_found', default='No matching results found.')
 label_advanced_search = _('label_advanced_search', default='Advanced Search&#8230;')
-label_show_all = _('label_show_all', default='Show all&#8230;')
+label_show_all = _('label_show_all', default='Show all items')
 
 ts = getToolByName(context, 'translation_service')
 
@@ -93,19 +96,19 @@ def write(s):
 
 if not results:
     write('''<fieldset class="livesearchContainer">''')
-#    write('''<legend id="livesearchLegend">%s</legend>''' % ts.translate(legend_livesearch))
+    #write('''<legend id="livesearchLegend">%s</legend>''' % ts.translate(legend_livesearch, context=REQUEST))
     write('''<span id="livesearchLegend"></span>''')
     write('''<div class="LSIEFix">''')
-    write('''<div id="LSNothingFound">%s</div>''' % ts.translate(label_no_results_found))
+    write('''<div id="LSNothingFound">%s</div>''' % ts.translate(label_no_results_found, context=REQUEST))
     write('''<div class="LSRow">''')
-    write('<a href="search_form" style="font-weight:normal">%s</a>' % ts.translate(label_advanced_search))
+    write('<a href="search_form" style="font-weight:normal">%s</a>' % ts.translate(label_advanced_search, context=REQUEST))
     write('''</div>''')
     write('''</div>''')
     write('''</fieldset>''')
 
 else:
     write('''<fieldset class="livesearchContainer">''')
-#    write('''<legend id="livesearchLegend">%s</legend>''' % ts.translate(legend_livesearch))
+    #write('''<legend id="livesearchLegend">%s</legend>''' % ts.translate(legend_livesearch, context=REQUEST))
     write('''<span id="livesearchLegend"></span>''')
     write('''<div class="LSIEFix">''')
     write('''<ul class="LSTable">''')
@@ -118,36 +121,32 @@ else:
         itemUrl = itemUrl + searchterm_query
 
         write('''<li class="LSRow">''')
-        if icon.url is not None and icon.description is not None:
-            write('''<img src="%s" alt="%s" width="%i" height="%i" />''' % (icon.url,
-                                                                            icon.description,
-                                                                            icon.width,
-                                                                            icon.height))
+        write(icon.html_tag() or '')
         full_title = safe_unicode(pretty_title_or_id(result))
         if len(full_title) > MAX_TITLE:
             display_title = ''.join((full_title[:MAX_TITLE],'...'))
         else:
             display_title = full_title
         full_title = full_title.replace('"', '&quot;')
-        write('''<a href="%s" title="%s">%s</a>''' % (itemUrl, full_title, display_title))
-        write('''<span class="discreet">[%s%%]</span>''' % result.data_record_normalized_score_)
+        klass = 'contenttype-%s' % ploneUtils.normalizeString(result.portal_type)
+        write('''<a href="%s" title="%s" class="%s">%s</a>''' % (itemUrl, full_title, klass, display_title))
         display_description = safe_unicode(result.Description)
         if len(display_description) > MAX_DESCRIPTION:
             display_description = ''.join((display_description[:MAX_DESCRIPTION],'...'))
         # need to quote it, to avoid injection of html containing javascript and other evil stuff
         display_description = html_quote(display_description)
-        write('''<div class="discreet" style="margin-left: 2.5em;">%s</div>''' % (display_description))
+        write('''<div class="LSDescr">%s</div>''' % (display_description))
         write('''</li>''')
         full_title, display_title, display_description = None, None, None
 
     write('''<li class="LSRow">''')
-    write( '<a href="search_form" style="font-weight:normal">%s</a>' % ts.translate(label_advanced_search))
+    write( '<a href="search_form" style="font-weight:normal">%s</a>' % ts.translate(label_advanced_search, context=REQUEST))
     write('''</li>''')
 
     if len(results)>limit:
         # add a more... row
         write('''<li class="LSRow">''')
-        write( '<a href="%s" style="font-weight:normal">%s</a>' % ('search?SearchableText=' + searchterms, ts.translate(label_show_all)))
+        write( '<a href="%s" style="font-weight:normal">%s</a>' % ('search?SearchableText=' + searchterms, ts.translate(label_show_all, context=REQUEST)))
         write('''</li>''')
     write('''</ul>''')
     write('''</div>''')
@@ -155,3 +154,4 @@ else:
 
 return '\n'.join(output).encode(site_encoding)
 
+# GW Canvis: Veure linies comentades. 
