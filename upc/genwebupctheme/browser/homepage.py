1e6b31e405b82b0e512f015dafe7a2e8b19c5135
Víctor Fernández de Alba <victor.fernandez@upcnet.es>
2011-08-24 13:39:49 +0200

    retocs i canvis en la ppal

--- upc/genwebupctheme/browser/homepage.py
+++ upc/genwebupctheme/browser/homepage.py
@@ -6,32 +6,33 @@ from upc.genwebupctheme.browser.interfaces import IHomepage
 from zope.interface import implements
 from Products.CMFCore.utils import getToolByName
 
+
 class HomePageView(BrowserView):
     implements(IHomepage)
-    
+
     def manageUrl(self):
         context_state = getMultiAdapter((self.context, self.request), name=u'plone_context_state')
         return '%s/@@manage-homeportlets' % context_state.view_url()
-        
+
     def canManagePortlets(self):
         mt = getToolByName(self.context, 'portal_membership')
-        return mt.checkPermission('Portlets: Manage portlets', self.context)    
+        return mt.checkPermission('Portlets: Manage portlets', self.context)
 
     def getFrontPage(self):
         """
         Funcio que retorna la pagina principal del espai. Te en compte els permissos de lusuari validat, amb un
-        restrictedTraverse sobre lobjecte (tenint en compte lidioma) 
+        restrictedTraverse sobre lobjecte (tenint en compte lidioma)
         """
         page = {}
         portal_state = getMultiAdapter((self.context, self.request),
                                         name=u'plone_portal_state')
-        portal=portal_state.portal()
-        
+        portal = portal_state.portal()
+
         FrontPageObj = portal.benvingut.getTranslation()
         idFrontPageObj = FrontPageObj.id
         traversal = portal.restrictedTraverse(idFrontPageObj)
-        page['body']=FrontPageObj.CookedBody()
-        
+        page['body'] = FrontPageObj.CookedBody()
+
         return page
 
     def getColumn1(self):
@@ -41,7 +42,7 @@ class HomePageView(BrowserView):
         gw_util = utils.getGWConfig(self)
         #gw_util = getUtility(IgenWebUtility, "GenWebControlPanelUtility")
         return gw_util.columna1
-        
+
     def getColumn2(self):
         """Funcio que agafa els valors de quines caixetes cal posar la
            home page del genweb
@@ -49,7 +50,7 @@ class HomePageView(BrowserView):
         gw_util = utils.getGWConfig(self)
         #gw_util = getUtility(IgenWebUtility, "GenWebControlPanelUtility")
         return gw_util.columna2
-                
+
     def getColumn3(self):
         """Funcio que agafa els valors de quines caixetes cal posar la
            home page del genweb
@@ -65,10 +66,9 @@ class HomePageView(BrowserView):
         portal_state = getMultiAdapter((self.context, self.request),
                                         name=u'plone_portal_state')
         portal = portal_state.portal()
-        return getattr(portal,'benvingut',False)
+        return getattr(portal, 'benvingut', False)
 
     def getSetupLink(self):
         """Funcio que dona l'enllas al formulari de creacio dels elements per defecte
         """
         return utils.portal_url(self) + "/setup-view"
-    
