ad59202cf665d680ede48b4fc7fcfcf983f0df57
Víctor Fernández de Alba <victor.fernandez@upcnet.es>
2011-10-24 12:15:45 +0200

    guarda per a que no peti el constraints en cas de que GW no estigui instalat al site

--- upc/genwebupctheme/events.py
+++ upc/genwebupctheme/events.py
@@ -3,10 +3,14 @@ from Products.CMFCore.interfaces import ISiteRoot
 from upc.genwebupctheme.browser import utils
 from upc.genwebupctheme.browser.interfaces import IgenWebControlPanelSchemaGeneral as gw_schema
 
-def folderAdded(folder,event):
+
+def folderAdded(folder, event):
     if ISiteRoot.providedBy(folder.aq_parent):
         folder.context = folder.aq_parent
         gw_util = utils.getGWConfig(folder)
+        if gw_util is None:
+            # El paquet de GW no esta instalat, no facis res
+            return
         folder.setConstrainTypesMode(1)
         folder.setLocallyAllowedTypes(tuple([i.value for i in gw_schema._v_attrs['constrains'].value_type.vocabulary.__iter__()]))
         folder.setImmediatelyAddableTypes(tuple(gw_util.constrains))
