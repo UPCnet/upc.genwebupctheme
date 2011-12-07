3995b1299a27aece2c76640ec6378f18c3c1de76
Ferran Pons <ferran.pons.sanche@upcnet.es>
2011-12-07 10:33:01 +0100

    Ara no apareixen les icones de Comparteix quan es una Intranet

--- upc/genwebupctheme/browser/addthis.py
+++ upc/genwebupctheme/browser/addthis.py
@@ -2,6 +2,7 @@
 
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
+from zope.component import getUtility, getMultiAdapter
 
 
  
@@ -13,9 +14,18 @@ class AddthisViewlet(ViewletBase):
         """ retorna True si estem a una Noticia
         """
         context = self.context
-        if context.portal_type == 'News Item' or context.portal_type == 'Document':
+        request = self.request
+        utils = getMultiAdapter((self.context, self.request),name=u'upc.genweb.utils')
+
+        #Si esta instalat el Genweb UPC llavors fem la comprovacio de si es una Intranet o no
+        #per no mostrar les icones del "Comparteix" en el cas de que sigui una Intranet.
+        if utils.getGWConfig() is not None:
+            if utils.getGWConfig().tipusNeutre2!=('Intranet'):
+                return True
+            else:
+                return False
+        else:
             return True
-        return True
     
     def retIdiomaActual(self):
         """ retorna l'idioma actual (p.ex. 'ca')
